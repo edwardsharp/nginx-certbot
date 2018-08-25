d6b5ec8fa21550935901178ec0be679ff1261f61
edward sharp <edward@edwardsharp.net>
2018-08-24 23:31:28 -0400

    base docker partz & demo

---
+++ Dockerfile
@@ -0,0 +1,32 @@
+FROM nginx:stable-alpine
+
+RUN apk add --update \
+    curl \
+    bash \
+    certbot \
+    tini \
+  && rm -rf /var/cache/apk/*
+
+RUN mkdir -p /var/www/letsencrypt/.well-known/acme-challenge
+
+COPY letsencrypt.conf /etc/nginx/snippets/letsencrypt.conf
+
+COPY ssl.conf /etc/nginx/snippets/ssl.conf
+
+EXPOSE 80
+
+EXPOSE 443
+
+VOLUME /etc/letsencrypt
+
+RUN mkdir -p /opt/letsencrypt/bin/
+
+COPY ./bin/ /opt/letsencrypt/bin/
+
+RUN ln -fs /opt/letsencrypt/bin/renew_certs.sh /etc/periodic/daily/
+
+WORKDIR /opt/letsencrypt/bin/
+
+ENTRYPOINT ["/sbin/tini", "--"]
+
+CMD ["entrypoint.sh", "crond", "-f"]
---
+++ bin/entrypoint.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+set -e
+
+# validate env varz.
+[[ -z "$DOMAINS" ]] && MISSING="$MISSING DOMAINS"
+[[ -z "$EMAIL" ]] && MISSING="$MISSING EMAIL"
+if [[ -n "$MISSING" ]]; then
+	echo "Missing required environment variables: $MISSING" >&2
+	exit 1
+fi
+
+# certificatez are separated by semi-colon (;). sub-domainz on each certificate are
+# separated by comma (,).
+CERTS=(${DOMAINS//;/ })
+
+# dry-run certbot first, if exit is ok (0) then omit --dry-run
+for DOMAINS in "${CERTS[@]}"; do
+	SUBDOMAINS=(${DOMAINS//,/ })
+	for DOMAIN in "${SUBDOMAINS[@]}"; do
+		# check if certz for $DOMAIN exist
+  	# "/etc/letsencrypt/live/${DOMAIN}/privkey.pem"
+  	if [ ! -f "/etc/letsencrypt/live/$DOMAIN/fullchain.pem" ]; then
+    	echo "running certbot for ${DOMAIN}"
+	  	certbot certonly \
+				--agree-tos \
+				-d "$DOMAIN" \
+				--email "$EMAIL" \
+				--expand \
+				--noninteractive \
+				--standalone \
+				--preferred-challenges http \
+				--dry-run \
+				$OPTIONS
+
+			EXITSTATUS=$?
+			if [ $EXITSTATUS -eq 0 ]; then
+				echo "dry run success! fetching cert for $DOMAINS"
+				certbot certonly \
+					--agree-tos \
+					-d "$DOMAIN" \
+					--email "$EMAIL" \
+					--expand \
+					--noninteractive \
+					--standalone \
+					--preferred-challenges http \
+					$OPTIONS
+			fi
+		fi
+	done
+done
+
+exec "$@"
